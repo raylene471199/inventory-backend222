3a4a12bc487c622c863420027aae20e12e793549
raylene471199 <13335032277@126.com>
2026-05-06 21:50:24 +0800

    Initialize routers by importing modules

---
+++ app/routers/__init__.py
@@ -0,0 +1,5 @@
+from .products import router as products_router
+from .stock_in import router as stock_in_router
+from .stock_out import router as stock_out_router
+from .stats import router as stats_router
+from .export import router as export_router
